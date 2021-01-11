fc1defeea07421d8eb08eb19c10b69592821bae6
ZhaoShaoyangGitHub <17621091909@163.com>
2021-01-11 22:15:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h6>{{test}}</h6>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    let test = ref(0);
+    test.value = 222;
+    return {
+      test
+    }
+  },
+  created() {
+    console.log(this.$)
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
